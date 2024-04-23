d1b3fb5b1c0bed9f9a396a199eca910aaa48cafa
Roberto Schneider <roberto.schneider@gmail.com>
2024-04-23 12:49:18 -0300

    pag. ok. incio dos testes

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(page_title="Streamlit + Docker", layout="wide")
+
+st.markdown("""
+    <style>
+        .reportview-container {
+            margin-top: -2em;
+        }
+        #MainMenu {visibility: hidden;}
+        .stDeployButton {display:none;}
+        footer {visibility: hidden;}
+        #stDecoration {display:none;}
+    </style>
+""", unsafe_allow_html=True)
+
+st.header('Meu site streamlit')
